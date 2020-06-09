d16a086be6b16c17397c3a5bc7550c8c4cfbb0ef
Alex Glover <aglover@zendesk.com>
2020-06-09 12:46:32 -0500

    moves installation and PATH setup to BASH script

---
+++ install-jsonnet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+echo "Downloading Jsonnet from: $1"
+wget --quiet $1 -O jsonnet.tar.gz
+
+mkdir bin
+tar xvf jsonnet.tar.gz --directory bin
+rm -f jsonnet.tar.gz
+
+# Add executables to the path for future actions
+echo "::add-path::$(pwd)/bin"
