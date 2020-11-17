07849017696df82fe786f594c88386bc5a8abbd8
Jesse-Cameron <jessec.cameron@gmail.com>
2020-11-17 11:12:09 +1100

    update jsonnet path

--- install-jsonnet.sh
+++ install-jsonnet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
 echo "Downloading Jsonnet from: $1"
-wget --quiet $1 -O jsonnet.tar.gz
+wget --quiet "$1" -O jsonnet.tar.gz
 
 mkdir -p bin
 tar xvf jsonnet.tar.gz --directory bin
 rm -f jsonnet.tar.gz
 
-# Add executables to the path for future actions
-echo "::add-path::$(pwd)/bin"
+# Add jsonnet executables to the path for future actions
+echo "$(pwd)/bin" >> "$GITHUB_PATH"
