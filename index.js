51d4d74652600240d81cb898653bd1ff516c3370
Andy Hsieh <shoukoo.koike@gmail.com>
2022-01-04 10:24:58 +1100

    Remove unused packages

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const core = require('@actions/core')
-const io = require('@actions/io')
 const { exec } = require('@actions/exec')
-const https = require('https')
 const path = require('path')
-
 
 const run = async () => {
   await exec(path.join(__dirname, 'install-jsonnet.sh'))
